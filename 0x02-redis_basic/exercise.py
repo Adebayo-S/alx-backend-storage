b9faa5d74d37fc03f7d290f73da79659fe86534e
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-18 23:57:07 -0500

    [feat]: get methods to retrieve data from redis and convert to desired format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Writing strings to Redis"""
+"""Writing, Reading, Incrementing values to Redis"""
 import uuid
 import redis
-from typing import Union
+from typing import Optional, Callable, Union
 
 
 class Cache:
@@ -14,7 +14,24 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """takes data arg, generates random key, store data in redis with random key"""
+        """takes data arg, generates random key,
+        store data in redis with random key"""
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str,
+            fn:  Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """takes key and convert data back to desired format"""
+        result = self._redis.get(key)
+        if fn:
+            return fn(result)
+        return result
+
+    def get_str(self, result: bytes) -> str:
+        """convert result to string"""
+        return str(result, 'UTF-8')
+
+    def get_int(self, result: bytes) -> int:
+        """convert result to int"""
+        return int.from_bytes(result, "big")
