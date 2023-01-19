0936a049e3fa60f93979e3d22815e3e8763f24df
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-19 00:44:51 -0500

    [feat]: count_calls function to count how many times mthods of the Cache class are called

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,20 @@
 import uuid
 import redis
 from typing import Optional, Callable, Union
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """decorator function"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """method wrapper"""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+
+    return wrapper
 
 
 class Cache:
@@ -13,6 +27,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes data arg, generates random key,
         store data in redis with random key"""
