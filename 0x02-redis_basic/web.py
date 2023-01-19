ba4732f08ada8d0b27ec2931a029a7e69c9a72b0
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-19 02:11:00 -0500

    [feat]: implemented an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+
+_redis = redis.Redis()
+
+
+def count_request(method: Callable) -> Callable:
+    """Count number of request sent to a URL"""
+
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """Wrapper function for decorator"""
+        url = str(*args)
+        _redis.incr("count:{}".format(url))
+        cache = _redis.get("count:{}".format(url))
+
+        if cache:
+            return cache.decode('utf-8')
+        else:
+            html = method(url)
+            _redis.setex("count:".format(url), 10, html)
+        return html
+
+    return wrapper
+
+
+@count_request
+def get_page(url: str) -> str:
+    """Obtain HTML content through URL"""
+    res = requests.get(url)
+    return res.text
